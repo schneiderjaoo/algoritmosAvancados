eeab67e757226abd192343769c8611c1d1274be2
João Schneider <126828193+schneiderjaoo@users.noreply.github.com>
2025-03-11 19:58:04 -0300

    Update db.sql Atualizado  db

--- backend/src/config/db.sql
+++ backend/src/config/db.sql
@@ -1,7 +1,8 @@
 CREATE TABLE usuarios (
-    nome_usuario VARCHAR(50) UNIQUE NOT NULL PRIMARY KEY,
-    senha_hash VARCHAR(255) NOT NULL,
-    email VARCHAR(100) UNIQUE NOT NULL
+    nome VARCHAR(50) UNIQUE NOT NULL PRIMARY KEY,
+    senha VARCHAR(255) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    data_criacao DATETIME
 );
 /
 --\dt se rodar no terminal vai poder ver as tabelas criadas
